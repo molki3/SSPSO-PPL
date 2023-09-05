3cda9eae352eb658be6f318bc431bfa23b1250f0
imjahirrivera <142928409+imjahirrivera@users.noreply.github.com>
2023-09-05 09:31:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ function enter() {
     }
     window.location.href = './procesos/procesos.html?procesos=' + procesos;
 };
+
+function soloNumeros(input) {
+    var valor = input.value;
+    var patron = /^[0-9]+$/;
+
+    if (!patron.test(valor)) {
+        input.value = valor.slice(0, -1); // Eliminar el último carácter no válido
+    }
+};
